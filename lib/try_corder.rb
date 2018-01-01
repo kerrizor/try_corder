a21afc15855092cf958fa767008d487110ebca29
Kerri Miller <kerrizor@kerrizor.com>
2017-12-31 17:41:10 -0800

    Patch onto Object

--- lib/try_corder.rb
+++ lib/try_corder.rb
@@ -2,5 +2,14 @@ require "try_corder/recorder"
 require "try_corder/reporter"
 require "try_corder/version"
 
-module TryCorder
+module ObjectExtension
+  def try(*a, &b)
+    TryCorder::Recorder.count(self.class.to_s, caller(1,1).first)
+
+    super(*a, &b)
+  end
+end
+
+class Object
+  prepend ObjectExtension
 end
